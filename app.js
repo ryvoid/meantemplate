bea7139a5816cf7140ab381fe08e568684daced5
Florian Jakob <f.jakob@outlook.com>
2019-05-27 23:34:49 +0200

    add ejs and css file

--- app.js
+++ app.js
@@ -10,12 +10,15 @@ const port = process.env.PORT || 5000;
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/css', express.static(path.join(__dirname, '/root/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist/umd')));
+app.set('views', './root/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index', { title: 'template' });
 });
 
 app.listen(port, () => {
